fb7260521b9bec1354f22945acdd1b85ed7df210
Yupiel <stefan_mitrovic@outlook.com>
2021-03-24 11:42:30 +0100

    #3 Add main and test folder in in src of the project
    - add HealthApplication.kt (basic initialization of Spring Boot application)
    - add HealthApplicationTest.kt (preliminary to make folder visible in github and run tests later)
    - add application.properties (automatically generated by intellij)

---
+++ src/main/kotlin/de/yupiel/helth/HelthApplication.kt
@@ -0,0 +1,11 @@
+package de.yupiel.helth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HelthApplication
+
+fun main(args: Array<String>) {
+	runApplication<HelthApplication>(*args)
+}
---
+++ src/test/kotlin/de/yupiel/helth/HelthApplicationTests.kt
@@ -0,0 +1,10 @@
+package de.yupiel.helth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HelthApplicationTests {
+
+}
+
