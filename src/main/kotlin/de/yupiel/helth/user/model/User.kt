246d53d2137906c632f3dfb44effedd027bd6039
Yupiel <stefan_mitrovic@outlook.com>
2021-04-03 17:17:25 +0200

    remove DockerHub push & add score to user model

--- src/main/kotlin/de/yupiel/helth/user/model/User.kt
+++ src/main/kotlin/de/yupiel/helth/user/model/User.kt
@@ -1,5 +1,6 @@
 package de.yupiel.helth.user.model
 
+import java.math.BigInteger
 import java.time.LocalDate
 import java.util.*
 import javax.persistence.Column
@@ -18,6 +19,9 @@ class User(username: String, password: String) {
     @Column(name = "password", nullable = false, unique = false)
     var password: String
         private set
+    @Column(name = "score", nullable = false, unique = false)
+    var score: BigInteger
+        private set
     @Column(name = "creation_date", nullable = false, unique = false)
     var creationDate: LocalDate
         private set
@@ -26,16 +30,7 @@ class User(username: String, password: String) {
         this.id = UUID.randomUUID()
         this.username = username
         this.password = password
+        this.score = BigInteger("0")    //TODO add support in service and Controller
         this.creationDate = LocalDate.now()
-    }
-
-    constructor(
-        id: UUID,
-        username: String,
-        password: String,
-        creationDate: LocalDate
-    ) : this(username, password) {
-        this.id = id
-        this.creationDate = creationDate
     }
 }
